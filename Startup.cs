01aec1b84362dac2a8aad7db6e16a3d56c6fd3da
Robert Wood Jr <rdrwood@gmail.com>
2020-10-19 20:30:47 -0700

    Service added

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WoodCrafts.Website.Services;
 
 namespace WoodCrafts.Website
 {
@@ -24,6 +25,8 @@ namespace WoodCrafts.Website
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddTransient<JsonFileProductService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
